edb3d508180a6d7df7d4b161eac24ce3529180c6
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-12 10:29:17 +0700

    เปลี่ยน base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.kanfin.com/spch1_ci3';
+$config['base_url'] = 'http://hr.kanfin.com';
 //$config['base_url'] = 'hr.kanfin.com';
 
 /*
