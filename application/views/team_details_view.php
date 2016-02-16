3e6c9ca2c73a3821000a5fd26b6147990ed7c058
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-16 17:15:09 +0700

    แบบฟอร์มแนบไฟล์

--- application/views/team_details_view.php
+++ application/views/team_details_view.php
@@ -93,132 +93,135 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                 <!-- Main content -->
                 <section class="content">
-                      <div class="container"> 
-                    <div class="col-sm-4">
-                        <!--Box-->
-                        <div class="box box-primary direct-chat direct-chat-primary" >
-                            <div class="box-header with-border">
-                                <h3 class="box-title">ฝากข้อความ</h3>
+                    <div class="container"> 
+                        <div class="col-sm-4">
+                            <!--Box-->
+                            <div class="box box-primary direct-chat direct-chat-primary" >
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">ฝากข้อความ</h3>
 
-                                <div class="box-tools pull-right">
-                                    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-light-blue">3</span>
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
-                                        <i class="fa fa-comments"></i></button>
-                                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                                </div>
-                            </div>
-                            <!-- /.box-header -->
-                            <div class="box-body" style="height: 400px" >
-                                <!-- Conversations are loaded here -->
-                                <div class="direct-chat-messages">
-                                    <!-- Message. Default to the left -->
-                                    <div class="direct-chat-msg">
-                                        <div class="direct-chat-info clearfix">
-                                            <span class="direct-chat-name pull-left">Alexander Pierce</span>
-                                            <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-                                        </div>
-                                        <!-- /.direct-chat-info -->
-                                        <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
-                                        <div class="direct-chat-text">
-                                            Is this template really for free? That's unbelievable!
-                                        </div>
-                                        <!-- /.direct-chat-text -->
+                                    <div class="box-tools pull-right">
+                                        <span data-toggle="tooltip" title="3 New Messages" class="badge bg-light-blue">3</span>
+                                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
+                                            <i class="fa fa-comments"></i></button>
+                                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                                     </div>
-                                    <!-- /.direct-chat-msg -->
-
-                                    <!-- Message to the right -->
-                                    <div class="direct-chat-msg right">
-                                        <div class="direct-chat-info clearfix">
-                                            <span class="direct-chat-name pull-right">Sarah Bullock</span>
-                                            <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-                                        </div>
-                                        <!-- /.direct-chat-info -->
-                                        <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
-                                        <div class="direct-chat-text">
-                                            You better believe it!
-                                        </div>
-                                        <!-- /.direct-chat-text -->
-                                    </div>
-                                    <!-- /.direct-chat-msg -->
                                 </div>
-                                <!--/.direct-chat-messages-->
+                                <!-- /.box-header -->
+                                <div class="box-body" style="height: 400px" >
+                                    <!-- Conversations are loaded here -->
+                                    <div class="direct-chat-messages">
+                                        <!-- Message. Default to the left -->
+                                        <div class="direct-chat-msg">
+                                            <div class="direct-chat-info clearfix">
+                                                <span class="direct-chat-name pull-left">Alexander Pierce</span>
+                                                <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                                            </div>
+                                            <!-- /.direct-chat-info -->
+                                            <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
+                                            <div class="direct-chat-text">
+                                                Is this template really for free? That's unbelievable!
+                                            </div>
+                                            <!-- /.direct-chat-text -->
+                                        </div>
+                                        <!-- /.direct-chat-msg -->
 
-                                <!-- Contacts are loaded here -->
-                                <div class="direct-chat-contacts">
-                                    <ul class="contacts-list">
-                                        <li>
-                                            <a href="#">
-                                                <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg">
-
-                                                <div class="contacts-list-info">
-                                                    <span class="contacts-list-name">
-                                                        Count Dracula
-                                                        <small class="contacts-list-date pull-right">2/28/2015</small>
-                                                    </span>
-                                                    <span class="contacts-list-msg">How have you been? I was...</span>
-                                                </div>
-                                                <!-- /.contacts-list-info -->
-                                            </a>
-                                        </li>
-                                        <!-- End Contact Item -->
-                                    </ul>
-                                    <!-- /.contatcts-list -->
-                                </div>
-                                <!-- /.direct-chat-pane -->
-                            </div>
-                            <!-- /.box-body -->
-                            <div class="box-footer">
-                                <form action="#" method="post">
-                                    <div class="input-group">
-                                        <input type="text" name="message" placeholder="พิมพ์ข้อความ..." class="form-control">
-                                        <span class="input-group-btn">
-                                            <button type="submit" class="btn btn-primary btn-flat">ส่ง</button>
-                                        </span>
+                                        <!-- Message to the right -->
+                                        <div class="direct-chat-msg right">
+                                            <div class="direct-chat-info clearfix">
+                                                <span class="direct-chat-name pull-right">Sarah Bullock</span>
+                                                <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
+                                            </div>
+                                            <!-- /.direct-chat-info -->
+                                            <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
+                                            <div class="direct-chat-text">
+                                                You better believe it!
+                                            </div>
+                                            <!-- /.direct-chat-text -->
+                                        </div>
+                                        <!-- /.direct-chat-msg -->
                                     </div>
-                                </form>
-                            </div>
-                            <!-- /.box-footer-->
-                        </div>
-                        <!--/.Box-->
-                        
-                        <!--แนบไฟล์-->
-                            
-                            
-                        <!--.แนบไฟล์-->
-                    </div> <!--/.col-sm-5-->
-                    <div class="col-sm-8">   
-                        <div class="box box-default">
-            <div class="box-header with-border">
-              <h3 class="box-title">รายละเอียดงาน</h3>
+                                    <!--/.direct-chat-messages-->
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-              </div>
-              <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body" style="display: block;">
-              <iframe height="450" width="100%" src="http://ai.stanford.edu/~nilsson/QAI/qai.pdf"></iframe> 
-              <br>
-              
-              <div class="col-sm-5">
-                  <label>แนบไฟล์ใหม่:</label>
-                  <input  type="file" name="fileDoc"/> 
-              </div>
-              <div class="col-sm-3">
-                  <label>ลบไฟล์:</label>
-                 <br> <a href="#" class="btn btn-sm btn-default"><span class="fa fa-trash"></span></a>
-              </div>
-              
-            </div>
-            <!-- /.box-body -->
-          </div>
-                        
-                    </div> <!--/.col-sm--->
-                     </div> <!--/.Containner-->
+                                    <!-- Contacts are loaded here -->
+                                    <div class="direct-chat-contacts">
+                                        <ul class="contacts-list">
+                                            <li>
+                                                <a href="#">
+                                                    <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg">
+
+                                                    <div class="contacts-list-info">
+                                                        <span class="contacts-list-name">
+                                                            Count Dracula
+                                                            <small class="contacts-list-date pull-right">2/28/2015</small>
+                                                        </span>
+                                                        <span class="contacts-list-msg">How have you been? I was...</span>
+                                                    </div>
+                                                    <!-- /.contacts-list-info -->
+                                                </a>
+                                            </li>
+                                            <!-- End Contact Item -->
+                                        </ul>
+                                        <!-- /.contatcts-list -->
+                                    </div>
+                                    <!-- /.direct-chat-pane -->
+                                </div>
+                                <!-- /.box-body -->
+                                <div class="box-footer">
+                                    <form action="#" method="post">
+                                        <div class="input-group">
+                                            <input type="text" name="message" placeholder="พิมพ์ข้อความ..." class="form-control">
+                                            <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-primary btn-flat">ส่ง</button>
+                                            </span>
+                                        </div>
+                                    </form>
+                                </div>
+                                <!-- /.box-footer-->
+                            </div>
+                            <!--/.Box-->
+
+
+                        </div> <!--/.col-sm-5-->
+                        <div class="col-sm-8">   
+                            <div class="box box-default">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">รายละเอียดงาน</h3>
+
+                                    <div class="box-tools pull-right">
+                                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                        </button>
+                                    </div>
+                                    <!-- /.box-tools -->
+                                </div>
+                                <!-- /.box-header -->
+                                <div class="box-body" style="display: block;">
+                                    <iframe height="450" width="100%" src="http://ai.stanford.edu/~nilsson/QAI/qai.pdf"></iframe> 
+                                    <br>
+
+                                    <div class="col-sm-6">
+                                        <label>แนบไฟล์ใหม่:</label>
+                                        <?php echo form_open_multipart() ?>
+                                        <ul class="list-inline">
+                                            <li><input  type="file" name="fileDoc"/> </li>
+                                            <li><button title="อัพโหลด" type="submit" class="btn btn-sm btn-default"><span class="fa fa-upload"></span></button></li>
+                                        </ul>
+                                        
+                                        <?php echo form_close(); ?>
+                                    </div>
+                                    <div class="col-sm-offset-3 col-sm-3">
+                                        <label>ลบไฟล์:</label>
+                                        <br> <a href="#" class="btn btn-sm btn-default"><span class="fa fa-trash"></span></a>
+                                    </div>
+
+                                </div>
+                                <!-- /.box-body -->
+                            </div>
+
+                        </div> <!--/.col-sm--->
+                    </div> <!--/.Containner-->
 
 
 
